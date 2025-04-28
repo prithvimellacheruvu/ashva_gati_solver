3c15b01d41cca8300dc5930cb3c2681b84b86f2b
Prithvi Mellacheruvu <prithvi.mellacheruvu@neura-robotics.com>
2025-04-28 18:08:11 +0200

    First commit.

---
+++ ashva_gati_solver/__init__.py
@@ -0,0 +1,11 @@
+# ashva_gati/__init__.py
+
+# This file marks the directory as a Python package
+
+"""
+Ashva Gati - Knight's tour.
+Knight's tour in a chess board based on solutions of 18th Century Maharaj of Mysuru.
+
+Author: Prithvi Mellacheruvu
+License: MIT
+"""
